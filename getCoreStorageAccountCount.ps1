35055d2c754056442997bc998edffc6db2c768b6
Jason Poon <git@jasonpoon.ca>
2017-07-27 12:05:59 -0700

    Add scripts

---
+++ getCoreStorageAccountCount.ps1
@@ -0,0 +1,48 @@
+﻿<# 
+.DESCRIPTION
+Walksthrough resource groups in an Azure subscription and calculates the storage account and core count
+ 
+#>
+
+$allRGs = (Get-AzureRmResourceGroup).ResourceGroupName
+Write-Warning "Found $($allRGs | Measure-Object | Select -ExpandProperty Count) total RGs"
+
+foreach ($rgName in $allRGs)
+{
+    $rg = Get-AzureRmResourceGroup -Name $rgName
+    $resources = Find-AzureRmResource -ResourceGroupNameEquals $rgName
+
+    # Owner
+    $owner = "Unknown"
+    if ($rg.Tags)
+    {
+        $owner = $rg.Tags["CREATED-BY"]
+    }
+
+    $numResources = $resources | Measure-Object | Select -ExpandProperty Count
+    $isEmpty = $numResources -eq 0
+
+    # Storage Account
+    $numSA = $resources | Where {$_.ResourceType -eq "Microsoft.Storage/storageAccounts" } | Measure-Object | Select -ExpandProperty Count
+
+    # Cores
+    $numCores = 0
+
+    #   vm
+    $vmNames = $resources | Where {$_.ResourceType -eq "Microsoft.Compute/virtualMachines" } | Select -ExpandProperty Name
+    foreach ($vmName in $vmNames) {
+        $size = (Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName).HardwareProfile.VmSize
+        $numCores += Get-AzureRmVmSize -location $rg.Location | ?{ $_.name -eq $size } | Select -ExpandProperty NumberOfCores
+    }
+
+    #   vmss
+    $vmssNames = $resources | Where {$_.ResourceType -eq "Microsoft.Compute/virtualMachineScaleSets" } | Select -ExpandProperty Name
+    foreach ($vmssName in $vmssNames) {
+        $vmss = Get-AzureRmVmss -ResourceGroupName $rgName -VMScaleSetName $vmssName
+        $numCores += (Get-AzureRmVmSize -location $rg.Location | ?{ $_.name -eq $vmss.Sku.Name } | Select -ExpandProperty NumberOfCores) * $vmss.Sku.Capacity
+    }
+
+    Write-Host "$rgName, $rg.Location, $owner, $isEmpty, $numSA, $numCores"
+}
+
+
---
+++ tagOwnerLastDeployed.ps1
@@ -0,0 +1,51 @@
+﻿<# 
+.DESCRIPTION 
+Ascertains the owner of a resource group through Azure Event Logs.
+If an owner is found, a 'CREATED-BY' tag is applied to the resource group.
+ 
+.PARAMETER dryRun
+If true, changes will not be applied to Azure. The default is false.
+
+#>
+
+$allRGs = (Get-AzureRmResourceGroup).ResourceGroupName
+Write-Warning "Found $($allRGs | Measure-Object| Select-Object -ExpandProperty Count) total RGs"
+
+$aliasedRGs = (Find-AzureRmResourceGroup -Tag @{ "CREATED-BY" = $null }).Name
+Write-Warning "Found $($aliasedRGs | Measure-Object| Select-Object -ExpandProperty Count) tagged RGs"
+  
+$notAliasedRGs = $allRGs | ?{-not ($aliasedRGs -contains $_)}
+Write-Warning "Found $($notAliasedRGs | Measure-Object | Select-Object -ExpandProperty Count) un-tagged RGs"
+
+foreach ($rg in $notAliasedRGs)
+{
+    $currentTime = Get-Date
+    $endTime = $currentTime
+    $startTime = $endTime.AddDays(-7)
+        
+    $callers = Get-AzureRmLog -ResourceGroup $rg -StartTime $startTime -EndTime $endTime -WarningAction SilentlyContinue |
+        Where-Object {$_.Authorization.Action -eq "Microsoft.Resources/deployments/write" -or $_.Authorization.Action -eq "Microsoft.Resources/subscriptions/resourcegroups/write" } | 
+        Select-Object -ExpandProperty Caller | 
+        Group-Object | 
+        Sort-Object  | 
+        Select-Object -ExpandProperty Name
+
+    if ($callers)
+    {
+        $owner = $callers | Select-Object -First 1
+        $alias = $owner -replace "@microsoft.com",""
+
+        $tags = (Get-AzureRmResourceGroup -Name $rg).Tags
+        $tags += @{ "CREATED-BY"=$alias }
+
+        $rg + ", " + $alias
+        if (-not $dryRun) 
+        {
+            Set-AzureRmResourceGroup -Name $rg -Tag $tags
+        }
+    } 
+    else 
+    {
+        $rg + ", Unknown"
+    }   
+}
